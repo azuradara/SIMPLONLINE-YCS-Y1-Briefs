113f861b8ad0ca102906a4f615f534880f70adb1
Dara <dara-@outlook.jp>
2021-02-15 16:55:58 +0100

    res bas

---
+++ Brief [03]/js/reservation.js
@@ -0,0 +1,144 @@
+// suspEl = document.querySelector('#slc-transmission')
+const typeEl = document.querySelector('#slc-type')
+const engineEl = document.querySelector('#slc-engine')
+const daysEl = document.querySelector('#slc-days')
+const btn = document.querySelector('button')
+
+// const transArr = {
+// 	'Manual': ['Citadine', 'Compact', 'Utilitaire', 'Engin de Chantier'],
+// 	'Auto': ['Berline', 'Camion'],
+// 	'Other': ['Moto']
+// }
+
+const transArr = {
+	auto: {
+		text: "Automatique",
+		mod: .19
+	},
+	manual: {
+		text: "Manuelle",
+		mod: 0
+	},
+	none: {
+		text: "--",
+		mod: 0
+	}
+}
+
+const engineArr = {
+
+	'Electrique': .05,
+	
+	'Hybride': .09,
+	
+	'Essence': .14,
+
+	'Diesel': .21
+
+}
+
+const typeArr = {
+	'Moto': {
+		basePrice: 10,
+		transmission: transArr.none,
+		engine: ['Electrique', 'Essence']
+	},
+
+	'Citadine': {
+		basePrice: 12,
+		transmission: transArr.manual,
+		engine: ['Electrique', 'Hybride', 'Essence', 'Diesel']
+	},
+
+	'Compact': {
+		basePrice: 14,
+		transmission: transArr.manual,
+		engine: ['Hybride', 'Essence', 'Diesel']
+	},
+
+	'Berline': {
+		basePrice: 20,
+		transmission: transArr.auto,
+		engine: ['Hybride', 'Essence', 'Diesel']
+	},
+
+	'Utilitaire': {
+		basePrice: 16,
+		transmission: transArr.manual,
+		engine: ['Diesel']
+	},
+
+	'Engin de Chantier': {
+		basePrice: 900,
+		transmission: transArr.manual,
+		engine: ['Essence', 'Diesel']
+	},
+
+	'Camion': {
+		basePrice: 250,
+		transmission: transArr.auto,
+		engine: ['Diesel']
+	}
+}
+
+window.onload = function () {
+	populateOpts(typeEl, Object.keys(typeArr))
+	btn.disabled = true
+	btn.classList.add('btn--disabled')
+}
+
+function populateOpts(selecEl, arr) {
+	let filler = document.createElement('option')
+	filler.text = '--'
+	filler.value = null
+	selecEl.add(filler)
+
+	for (x in arr) {
+		let option = document.createElement('option')
+		option.text = arr[x]
+		option.value = arr[x]
+
+		selecEl.add(option)
+	}
+}
+
+function typeSelect() {
+	for (x in engineEl.options) {
+		engineEl.remove(x)
+	}
+	populateOpts(engineEl, typeArr[typeEl.value].engine)
+}
+// 
+
+class VehicleLease {
+	constructor(Type, Engine, Period) {
+		this.Type = typeArr[Type]
+		this.Engine = engineArr[Engine]
+		this.Period = Period
+		
+		this.Transmission = this.Type.transmission
+		this.basePrice = this.Type.basePrice
+	}
+}
+
+// 
+typeEl.onchange = function () {
+	typeSelect()
+}
+
+engineEl.onchange = function() {
+	if (engineEl.value != '--') {
+		btn.disabled = false
+		btn.classList.remove('btn--disabled')
+	}
+}
+
+function concoct() {
+	const sVeh = new VehicleLease(typeEl.value, engineEl.value, daysEl.value)
+	console.log(sVeh)
+
+	const pricePerDay = sVeh.basePrice + (sVeh.basePrice * sVeh.Transmission.mod) + (sVeh.basePrice * sVeh.Engine)
+	console.log(pricePerDay)
+
+	document.querySelector('.price').textContent = pricePerDay * sVeh.Period
+}
