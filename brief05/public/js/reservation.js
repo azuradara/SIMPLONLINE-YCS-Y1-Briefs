b2bc5ce42a410cf7a57756700b38f8c706592d2d
Dara <dara-@outlook.jp>
2021-04-22 15:00:41 +0000

    yawy

--- brief05/public/js/reservation.js
+++ brief05/public/js/reservation.js
@@ -1,25 +1,44 @@
-const inputs = document.querySelectorAll('input')
 const form = document.querySelector('.res__form')
 
-const bill = {
-	rm_type: null,
-}
+const bill = {}
 
 form.addEventListener('click', e => {
+	let inputs = document.querySelectorAll('input')
 
 	inputs.forEach(i => {
 		label = i.closest('label')
 
 		if (i.checked) {
-			bill.rm_type = i.value
+			bill[i.name] = i.value
 			label.classList.add('ring-yellow-500', 'bg-yellow-50', 'hover:bg-yellow-100')
 		} else {
 			label.classList.remove('ring-yellow-500', 'bg-yellow-50', 'hover:bg-yellow-100')
 		}
 	})
 
-	bill.rm_type ===
+	if (bill.rm_type === 'single') {
+		createSlc('rm', 'view', ['Interior', 'Exterior'], form)
+	}
 
-
-		console.log(bill)
+	console.log(bill)
 })
+
+const createSlc = (sub, name, opts = [], parentEl) => {
+	let slc = document.createElement('div')
+	slc.id = `${sub}_${name}`
+	slc.className = 'font-regular text-lg flex gap-5 items-center mx-auto w-max'
+
+	opts.forEach(o => {
+		slc.innerHTML += `
+	<label
+		class="ring-2 shadow-lg ring-gray-200 hover:bg-gray-100 cursor-pointer justify-between rounded-lg h-28 w-28 p-1 px-3 flex flex-col items-center"
+		for="${o}}">
+		<p class="text-sm font-semibold text-gray-400">${o}</p>
+		<input class="hidden" type="radio" id="${o}" name="${slc.id}" value="${o}">
+		<img class="h-10 w-10" src="img/res/${o}.svg" alt="">
+	</label>
+		`
+	})
+
+	parentEl.append(slc)
+}
