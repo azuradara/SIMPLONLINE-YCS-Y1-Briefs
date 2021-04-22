dcb021c360bf55659a09c490fd3be15a66aec8f3
Aymane D. Hlamnach <dara-@outlook.jp>
2021-04-22 08:05:53 +0000

    Update _main.php

--- brief05/views/layouts/_main.php
+++ brief05/views/layouts/_main.php
@@ -87,7 +87,7 @@ use app\core\Application;
 
 	</nav> -->
 
-	<nav class="bg-white shadow-xl mb-20 max-w-7xl mx-6 lg:mx-auto mt-6 rounded-xl">
+	<nav class="bg-white shadow-lg mb-20 max-w-7xl mx-6 lg:mx-auto mt-6 rounded-xl">
 		<div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
 			<div class="relative flex items-center justify-between h-16">
 				<div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
